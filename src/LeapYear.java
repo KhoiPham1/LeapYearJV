7d75a0d93e73309e4c490927b5f2c429baecb9d0
Khoi Pham <khoi171195@gmail.com>
2018-10-27 13:31:42 +0700

    firstcommit

---
+++ src/LeapYear.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter year");
+        int year = input.nextInt();
+        boolean isLeapYear = false;
+        if (year% 4 == 0){
+            if (year%100 ==0){
+                if (year%400 ==0){
+                    System.out.printf("%d is a leap year", year);
+                }else {
+                    System.out.printf("%d is not a leap year", year);
+                }
+            }else {
+                System.out.printf("%d is a leap year", year);
+            }
+        }else {
+            System.out.printf("%d is not a leap year", year);
+        }
+
+    }
+}
